58ed808f9ac3506c0c048e08cee06eae407a672b
Muspi Merol <wjj123345567@icloud.com>
2024-05-23 18:28:21 +0800

    fix: do not treat `.py` files as templates by default

--- promplate_compile/api/compile.py
+++ promplate_compile/api/compile.py
@@ -9,6 +9,6 @@ def compile_one(file: str | Path, *, encoding="utf-8"):
     Path(file).with_suffix(".py").write_text(template_to_code(Template.read(file, encoding=encoding)), encoding=encoding)
 
 
-def compile_all(folder: str | Path, pattern="*", *, encoding="utf-8"):
+def compile_all(folder: str | Path, pattern="*[!.py]", *, encoding="utf-8"):
     for file in Path(folder).rglob(pattern):
         compile_one(file, encoding=encoding)
